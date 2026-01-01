c4fc29456724ed4e97d466b2e8364e6f5736b8b1
Daniyl <mordikdanyyl@gmail.com>
2026-01-01 20:35:42 +0100

    Initial commit with 10 Python projects

--- To-Do_List/10-th rpoject. To-Do List.py
+++ To-Do_List/10-th rpoject. To-Do List.py
@@ -3,47 +3,47 @@ def to_do_list():
 
     while True:
         print("\nMenu:" \
-        "\n1.Show tasks." \
-        "\n2.Add a task." \
-        "\n3.Delete a task." \
-        "\n4.Exit." \
-        "\nYou choose: ")
+        "\n1. Show tasks." \
+        "\n2. Add a task." \
+        "\n3. Delete a task." \
+        "\n4. Exit." \
+        "\nYour choice: ")
 
         choice = input("Choose an option: ").strip()
 
         if choice == "1":
             if tasks:
-                print("Your tasks: ")
+                print("Your tasks:")
                 for i, task in enumerate(tasks, 1):
                     print(f"{i}. {task}")
             else:
-                print("No tasks are there.")
+                print("Your task list is empty.")
 
         elif choice == "2":
             task = input("Enter a task: ").strip()
             if task == "":
-                print("You didnt write anything")
+                print("You didn't write anything.")
             else:
                 tasks.append(task)
-                print(f"'{task}' has been successfully added to the list.")
+                print(f"'{task}' has been successfully added to your list.")
 
         elif choice == "3":
             try:
                 removed_task = int(input("Enter the task number you want to remove: "))
             except ValueError:
-                print("You have to write an integer")
+                print("Please enter an integer.")
             else:
                 if 0 < removed_task <= len(tasks):
-                    tasks.pop(removed_task - 1)
-                    print(f"The task was successfully removed.")
+                    removed = tasks.pop(removed_task - 1)
+                    print(f"'{removed}' has been removed from your list.")
                 else:
-                    print("Invalid number.")
+                    print("Invalid task number.")
 
         elif choice == "4":
             print("Goodbye!")
             break
 
         else:
-            print("Invalid option")
-             
+            print("Invalid option.")
+
 to_do_list()
